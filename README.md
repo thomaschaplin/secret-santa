658ec9d5b0c4360e844027f3c3d3bc1d3259f2ba
Thomas Chaplin <thomaschaplin@outlook.com>
2023-10-25 22:15:24 +0100

    updates

--- README.md
+++ README.md
@@ -1,6 +1,109 @@
 # Secret Santa
 
-Check it live on [arcanis.github.io/secretsanta/](http://arcanis.github.io/secretsanta/).
+![Secret Santa Logo](./assets/santa.png)
+
+Welcome to Secret Santa, a web application that simplifies the process of pairing participants for your Secret Santa gift exchange. This tool allows you to set up your Secret Santa event with specific rules and exclusions to make the holiday gift exchange even more fun and exciting. This is a clone of the original repo [arcanis/secretsanta](https://github.com/arcanis/secretsanta) with some customisation and changes.
+
+## Table of Contents
+
+- [Secret Santa](#secret-santa)
+  - [Table of Contents](#table-of-contents)
+  - [Demo](#demo)
+  - [Getting Started](#getting-started)
+  - [How to Use This Tool](#how-to-use-this-tool)
+  - [Customization](#customization)
+  - [Contributing](#contributing)
+  - [License (MIT)](#license-mit)
+
+## Demo
+
+If you want to see a demo of this Secret Santa generator in action, you can visit the website hosted at [www.thomaschaplin.me/secretsanta](www.thomaschaplin.me/secretsanta)
+
+## Getting Started
+
+To get started, you can follow these steps to run the Secret Santa Generator:
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/your-username/secret-santa.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd secret-santa
+   ```
+
+3. Open the `index.html` file in your web browser.
+
+That's it! You're now ready to start generating Secret Santa pairings for your event.
+
+## How to Use This Tool
+
+1. **Enter Participant Names:**
+
+   Start by entering the names of the people who will be participating in your Secret Santa event, one per line. You can add participants with or without additional details using parentheses.
+
+   Example:
+   - Thomas
+   - Frank (The Elf)
+
+2. **Set Exclusions:**
+
+   You can prevent someone from being paired with another participant by using the `!` symbol.
+
+   Example:
+   - Jane !Robert
+   - Robert !Jane
+
+3. **Multiple Exclusions:**
+
+   If you need to exclude a participant from being paired with multiple people, you can do so by listing all exclusions on a single line. However, be cautious with this, as too many exclusion rules can limit the possible pairings and make your Secret Santa less interesting.
+
+   Example:
+   - Thomas !Jane !Frank (The Elf)
+
+4. **Force Pairing:**
+
+   You can also specify pairings if you have a specific pairing in mind. Use the `=` symbol to force participants to be paired together.
+
+   Example:
+   - Thomas =Frank (The Elf)
+
+## Customization
+
+You can customise the Secret Santa generator by modifying the code in this repository. Feel free to add new features, improve the user interface, or enhance the pairing algorithm to better suit your needs. Contributions are always welcome!
+
+## Contributing
+
+If you want to contribute to this project, please follow these steps:
+
+1. Fork the repository to your own GitHub account.
+
+2. Create a new branch with a descriptive name:
+
+   ```bash
+   git checkout -b feature/my-feature
+   ```
+
+3. Make your changes and commit them:
+
+   ```bash
+   git commit -m "Add a new feature"
+   ```
+
+4. Push your changes to your forked repository:
+
+   ```bash
+   git push origin feature/my-feature
+   ```
+
+5. Open a pull request from your fork to this repository, explaining the changes you've made.
+
+6. Your pull request will be reviewed, and once accepted, your changes will be merged into the main project.
+
+Have a fun and festive Secret Santa event with the Secret Santa Generator!
 
 ## License (MIT)
 
--- SecretSanta.js
+++ SecretSanta.js
@@ -1,111 +1,82 @@
-var SecretSanta = function () {
-
+class SecretSanta {
+  constructor() {
     this.names = [];
+    this.enforced = Object.create(null);
+    this.blacklists = Object.create(null);
+  }
 
-    this.enforced = Object.create( null );
-    this.blacklists = Object.create( null );
-};
+  add(name) {
+    if (this.names.includes(name)) {
+      throw new Error(`Cannot redefine ${name}`);
+    }
 
+    this.names.push(name);
 
-SecretSanta.prototype.add = function ( name ) {
-
-    if ( this.names.indexOf( name ) !== -1 )
-        throw new Error( 'Cannot redefine ' + name );
-
-    this.names.push( name );
-
-    var subapi = { };
-
-    subapi.enforce = function ( other ) {
-
-        this.enforced[ name ] = other;
-
+    const subapi = {
+      enforce: (other) => {
+        this.enforced[name] = other;
         return subapi;
-
-    }.bind( this );
-
-    subapi.blacklist = function ( other ) {
-
-        if ( ! Object.prototype.hasOwnProperty.call( this.blacklists, name ) )
-            this.blacklists[ name ] = [];
-
-        if ( this.blacklists[ name ].indexOf( other ) === -1 )
-            this.blacklists[ name ].push( other );
-
-        return subapi;
-
-    }.bind( this );
-
-    return subapi;
-
-};
-
-SecretSanta.prototype.generate = function () {
-
-    var pairings = Object.create( null );
-    var candidatePairings = Object.create( null );
-
-    this.names.forEach( function ( name ) {
-
-        if ( Object.prototype.hasOwnProperty.call( this.enforced, name ) ) {
-
-            var enforced = this.enforced[ name ];
-
-            if ( this.names.indexOf( enforced ) === -1 )
-                throw new Error( name + ' is paired with ' + enforced + ', which hasn\'t been declared as a possible pairing' );
-
-            Object.keys( pairings ).forEach( function ( name ) {
-
-                if ( pairings[ name ] === enforced ) {
-                    throw new Error( 'Per your rules, multiple persons are paired with ' + enforced );
-                }
-
-            } );
-
-            candidatePairings[ name ] = [ this.enforced[ name ] ];
-
-        } else {
-
-            var candidates = _.difference( this.names, [ name ] );
-
-            if ( Object.prototype.hasOwnProperty.call( this.blacklists, name ) )
-                candidates = _.difference( candidates, this.blacklists[ name ] );
-
-            candidatePairings[ name ] = candidates;
-
+      },
+      blacklist: (other) => {
+        if (!this.blacklists[name]) {
+          this.blacklists[name] = [];
         }
-
-    }, this );
-
-    var findNextGifter = function () {
-
-        var names = Object.keys( candidatePairings );
-
-        var minCandidateCount = _.min( names.map( function ( name ) { return candidatePairings[ name ].length; } ) );
-        var potentialGifters = names.filter( function ( name ) { return candidatePairings[ name ].length === minCandidateCount; } );
-
-        return _.sample( potentialGifters );
-
+        if (!this.blacklists[name].includes(other)) {
+          this.blacklists[name].push(other);
+        }
+        return subapi;
+      },
     };
 
-    while ( Object.keys( candidatePairings ).length > 0 ) {
+    return subapi;
+  }
 
-        var name = findNextGifter();
+  generate() {
+    const pairings = Object.create(null);
+    const candidatePairings = Object.create(null);
 
-        if ( candidatePairings[ name ].length === 0 )
-            throw new Error('We haven\'t been able to find a match for ' + name + '! Press "Generate" to try again and, if it still doesn\'t work, try removing some exclusions from your rules. Sorry for the inconvenience!');
+    this.names.forEach((name) => {
+      if (this.enforced[name]) {
+        const enforced = this.enforced[name];
+        if (!this.names.includes(enforced)) {
+          throw new Error(`${name} is paired with ${enforced}, which hasn't been declared as a possible pairing`);
+        }
+        for (const pairedName in pairings) {
+          if (pairings[pairedName] === enforced) {
+            throw new Error(`Per your rules, multiple persons are paired with ${enforced}`);
+          }
+        }
+        candidatePairings[name] = [this.enforced[name]];
+      } else {
+        let candidates = this.names.filter((n) => n !== name);
+        if (this.blacklists[name]) {
+          candidates = candidates.filter((n) => !this.blacklists[name].includes(n));
+        }
+        candidatePairings[name] = candidates;
+      }
+    });
 
-        var pairing = _.sample( candidatePairings[ name ] );
-        delete candidatePairings[ name ];
+    const findNextGifter = () => {
+      const names = Object.keys(candidatePairings);
+      const minCandidateCount = Math.min(...names.map((n) => candidatePairings[n].length));
+      const potentialGifters = names.filter((n) => candidatePairings[n].length === minCandidateCount);
+      return potentialGifters[Math.floor(Math.random() * potentialGifters.length)];
+    };
 
-        Object.keys( candidatePairings ).forEach( function ( name ) {
-            candidatePairings[ name ] = _.without( candidatePairings[ name ], pairing );
-        } );
+    while (Object.keys(candidatePairings).length > 0) {
+      const name = findNextGifter();
+      if (candidatePairings[name].length === 0) {
+        throw new Error(`We haven't been able to find a match for ${name}! Press "Generate" to try again and, if it still doesn't work, try removing some exclusions from your rules. Sorry for the inconvenience!`);
+      }
+      const pairing = candidatePairings[name][Math.floor(Math.random() * candidatePairings[name].length)];
+      delete candidatePairings[name];
 
-        pairings[ name ] = pairing;
-
+      for (const pairedName in candidatePairings) {
+        candidatePairings[pairedName] = candidatePairings[pairedName].filter((n) => n !== pairing);
+      }
+      pairings[name] = pairing;
     }
 
     return pairings;
-
-};
+  }
+}
